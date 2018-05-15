cc7ceb92d21542e6303cad7a12ca3599523a67ca
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-05-15 18:57:26 +0100

    es lint fuck you

--- Project/disney/src/router/index.js
+++ Project/disney/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Profile from '@/components/Profile'
 import Decade from '@/components/Decade'
 
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     },
     {
       path: '/',
