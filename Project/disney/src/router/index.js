a3b9655f666a005812653d7a222286ac6c7417a8
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-05-15 18:51:40 +0100

    blank pages created

--- Project/disney/src/router/index.js
+++ Project/disney/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Profile from '@/components/Profile'
+import Decade from '@/components/Decade'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/',
+      name: 'Decade',
+      component: Decade
     }
   ]
 })
