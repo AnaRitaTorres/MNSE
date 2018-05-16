44f5ee3c1ca9b709a5f8a494f5433a64792219ef
Jonas L <joaohccl@hotmail.com>
2018-05-16 15:49:55 +0100

    rip vuetify, bootstrap is my new best friend

--- Project/disney/src/components/Home.vue
+++ Project/disney/src/components/Home.vue
@@ -1,29 +1,86 @@
 <template>
-<div>
-    <v-card height="200px" flat>
-    <div class="headline text-xs-center pa-5">Active: {{ e1 }}</div>
-    <v-bottom-nav :value="true" :active.sync="e1" absolute color="transparent">
-      <v-btn flat color="teal" value="recent">
-        <span>Recent</span>
-        <v-icon>history</v-icon>
-      </v-btn>
-      <v-btn flat color="teal" value="favorites">
-        <span>Favorites</span>
-        <v-icon>favorite</v-icon>
-      </v-btn>
-      <v-btn flat color="teal" value="nearby">
-        <span>Nearby</span>
-        <v-icon>place</v-icon>
-      </v-btn>
-    </v-bottom-nav>
-  </v-card>
-</div>
+  <div class="cois">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="exampleInputGroup1"
+                    label="Email address:"
+                    label-for="exampleInput1"
+                    description="We'll never share your email with anyone else.">
+        <b-form-input id="exampleInput1"
+                      type="email"
+                      v-model="form.email"
+                      required
+                      placeholder="Enter email">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup2"
+                    label="Your Name:"
+                    label-for="exampleInput2">
+        <b-form-input id="exampleInput2"
+                      type="text"
+                      v-model="form.name"
+                      required
+                      placeholder="Enter name">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup3"
+                    label="Food:"
+                    label-for="exampleInput3">
+        <b-form-select id="exampleInput3"
+                       :options="foods"
+                       required
+                       v-model="form.food">
+        </b-form-select>
+      </b-form-group>
+      <b-form-group id="exampleGroup4">
+        <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: 'Select One', value: null },
+        'Carrots', 'Beans', 'Tomatoes', 'Corn'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset (evt) {
+      evt.preventDefault()
+      /* Reset our form values */
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false
+      this.$nextTick(() => { this.show = true })
+    }
+  }
 }
 </script>
-
-<style scoped>
+<style>
+  .cois{
+    padding: 20px;
+  }
 </style>
