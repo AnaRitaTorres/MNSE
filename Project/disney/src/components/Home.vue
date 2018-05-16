59e7445e91d554f13a8813572471dec23634c0b9
Jonas L <joaohccl@hotmail.com>
2018-05-16 18:38:29 +0100

    wheel of fortune

--- Project/disney/src/components/Home.vue
+++ Project/disney/src/components/Home.vue
@@ -1,86 +1,46 @@
 <template>
-  <div class="cois">
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="exampleInputGroup1"
-                    label="Email address:"
-                    label-for="exampleInput1"
-                    description="We'll never share your email with anyone else.">
-        <b-form-input id="exampleInput1"
-                      type="email"
-                      v-model="form.email"
-                      required
-                      placeholder="Enter email">
-        </b-form-input>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup2"
-                    label="Your Name:"
-                    label-for="exampleInput2">
-        <b-form-input id="exampleInput2"
-                      type="text"
-                      v-model="form.name"
-                      required
-                      placeholder="Enter name">
-        </b-form-input>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup3"
-                    label="Food:"
-                    label-for="exampleInput3">
-        <b-form-select id="exampleInput3"
-                       :options="foods"
-                       required
-                       v-model="form.food">
-        </b-form-select>
-      </b-form-group>
-      <b-form-group id="exampleGroup4">
-        <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
+  <div id="homeBackground">
+    <b-img src="./static/images/disney logo.jpg" fluid alt="Responsive disney image" />
+    <div id="wheelOutside">
+      <b-img center id="wheel" src="./static/images/wheel.png" fluid alt="Responsive wheel image" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      form: {
-        email: '',
-        name: '',
-        food: null,
-        checked: []
-      },
-      foods: [
-        { text: 'Select One', value: null },
-        'Carrots', 'Beans', 'Tomatoes', 'Corn'
-      ],
-      show: true
-    }
-  },
-  methods: {
-    onSubmit (evt) {
-      evt.preventDefault()
-      alert(JSON.stringify(this.form))
-    },
-    onReset (evt) {
-      evt.preventDefault()
-      /* Reset our form values */
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      /* Trick to reset/clear native browser form validation state */
-      this.show = false
-      this.$nextTick(() => { this.show = true })
-    }
-  }
 }
 </script>
 <style>
-  .cois{
+  #homeBackground{
+    position: relative;
     padding: 20px;
+    left: 0;
+    right: 0;
+  }
+  #wheelOutside{
+    /*position: absolute;*/
+    left: 0;
+    right: 0;
+    width: 50%;
+    height: 50%;
+    top: 25%;
+    margin: 0 auto;
+    position: absolute;
+  }
+  #wheel{
+    animation-name: example;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+  }
+  /* Safari 4.0 - 8.0 */
+  @-webkit-keyframes example {
+    0%   {transform: rotate(0deg);}
+    100% {transform: rotate(360deg);}
+  }
+  /* Standard syntax */
+  @keyframes example {
+    0%   {transform: rotate(0deg);}
+    100% {transform: rotate(360deg);}
   }
 </style>
