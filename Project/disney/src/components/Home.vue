062f704639e1e76b3d2aac7da316b24f79d92c05
Jonas L <joaohccl@hotmail.com>
2018-05-16 19:36:19 +0100

    hmm progress

--- Project/disney/src/components/Home.vue
+++ Project/disney/src/components/Home.vue
@@ -2,13 +2,21 @@
   <div id="homeBackground">
     <b-img src="./static/images/disney logo.jpg" fluid alt="Responsive disney image" />
     <div id="wheel">
-      <b-img center id="wheelOutside" src="./static/images/wheel.png" fluid alt="Responsive wheel image" />
+      <b-img v-if="coiso" center id="wheelOutside" src="./static/images/wheel.png" fluid alt="Responsive wheel image" />
+      <div id="wheel2">
+        <b-img id="char1" class="char" src="./static/images/char1.png" fluid alt="char1"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      coiso: true
+    }
+  }
 }
 </script>
 <style>
@@ -27,8 +35,28 @@ export default {
     top: 25%;
     margin: 0 auto;
     position: absolute;
-    -webkit-clip-path: polygon(0 0, 100% 0, 100% 50%, 0% 50%);
-    clip-path: polygon(0 0, 100% 0, 100% 88%, 0% 88%);
+    /*-webkit-clip-path: polygon(0 0, 100% 0, 100% 50%, 0% 50%);*/
+    /*clip-path: polygon(0 0, 100% 0, 100% 88%, 0% 88%);*/
+  }
+  #wheel2{
+    /*position: absolute;*/
+    left: 0;
+    right: 0;
+    /*width: 100%;*/
+    /*height: 200%;*/
+    top: -100%;
+    margin: 0 auto;
+    position: relative;
+    /*-webkit-clip-path: polygon(0 0, 100% 0, 100% 50%, 0% 50%);*/
+    /*clip-path: polygon(0 0, 100% 0, 100% 88%, 0% 88%);*/
+  }
+  .char{
+    z-index: 10;/*hmm*/
+    width: 10%;
+    height: 10%;
+  }
+  #char1{
+    position: relative;
   }
   #wheelOutside{
     animation-name: example;
