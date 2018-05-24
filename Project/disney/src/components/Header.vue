6233cdb0031cef0076719e8295bf97426eb64332
Jonas L <joaohccl@hotmail.com>
2018-05-24 03:16:51 +0100

    started modal

--- Project/disney/src/components/Header.vue
+++ Project/disney/src/components/Header.vue
@@ -6,9 +6,36 @@
             </b-navbar-brand>
             <b-navbar-nav class="ml-auto">
                 <b-collapse is-nav id="nav_collapse">
-                    <b-nav-item href="#">
-                        Home
-                    </b-nav-item>
+                    <b-button @click="showModal">
+                        Open Modal
+                    </b-button>
+                    <b-modal ref="myModalRef" hide-footer title="Log In or Register">
+                        <div class="d-block text-center">
+                            <h3>Log In</h3>
+                            <b-form-group
+                                id="fieldset1"
+                                label="Enter your email"
+                                label-for="input1"
+                                :state="state"
+                            >
+                                <b-form-input id="input1" :state="state" v-model.trim="username"></b-form-input>
+                            </b-form-group>
+                            <b-form-group
+                              id="fieldset2"
+                              label="Enter your password"
+                              label-for="input2"
+                              :invalid-feedback="invalidFeedback"
+                              :valid-feedback="validFeedback"
+                              :state="state"
+                            >
+                              <b-form-input id="input2" :state="state" v-model.trim="password"></b-form-input>
+                            </b-form-group>
+                        </div>
+                        <br> <!-- ñ havia uma class/tag que punha um tracinho? -->
+                        <div class="d-block text-center">
+                            <h3>Register</h3>
+                        </div>
+                    </b-modal>
                     <b-nav-form>
                         <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
                     </b-nav-form>
@@ -20,6 +47,32 @@
 
 <script>
 export default {
+  methods: {
+    showModal () {
+      this.$refs.myModalRef.show()
+    }
+  },
+  computed: {
+    state () {
+      return this.password.length >= 1 && this.username.length >= 1
+    },
+    invalidFeedback () {
+      if (this.password.length > 1 && this.username.length >= 1) {
+        return ''
+      } else {
+        return 'Password or username field empty!'
+      }
+    },
+    validFeedback () {
+      return this.state === true ? 'Thank you' : ''
+    }
+  },
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
 }
 </script>
 
