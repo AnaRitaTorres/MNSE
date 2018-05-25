332e1038fc54f9d2322a7c575e46b81060057a37
Jonas L <joaohccl@hotmail.com>
2018-05-25 05:33:47 +0100

    modal for chars and movies done!

--- Project/disney/src/components/Profile.vue
+++ Project/disney/src/components/Profile.vue
@@ -71,15 +71,20 @@
           <b-card id="film_info">
             <b-row>
               <b-col v-for="item in displayMovies" :key="item.pic + '' + 'mov'" >
-                <b-img v-bind:src="dbURL + 'static/movies/' + item.pic" fluid alt="Responsive image" />
+                <b-img v-bind:src="dbURL + 'static/movies/' + item.pic" @click="showModal3(item.name, item.description)" fluid alt="Responsive image" />
               </b-col>
+              <b-modal ref="myModalRef3" hide-footer v-bind:title="coiso.name">
+                <div class="d-block text-md-justify">
+                  {{ coiso.description }}
+                </div>
+              </b-modal>
             </b-row>
           </b-card>
           <h4>Favourite Characters</h4>
           <b-card id="character_info">
             <b-row>
               <b-col v-for="item in displayCharacters" :key="item.pic + '' + 'char'">
-                <b-img v-bind:src="dbURL + 'static/characters/' + item.pic" fluid alt="Responsive image" />
+                <b-img v-bind:src="dbURL + 'static/characters/' + item.pic" @click="showModal3(item.name, item.description)" fluid alt="Responsive image" />
               </b-col>
             </b-row>
           </b-card>
@@ -119,7 +124,11 @@ export default {
       canFollow: true,
       isFollower: false,
       curFollower: 0,
-      curFollowing: 0
+      curFollowing: 0,
+      coiso: {
+        name: '',
+        description: ''
+      }
     }
   },
   methods: {
@@ -160,6 +169,11 @@ export default {
     },
     showModal2 () {
       this.$refs.myModalRef2.show()
+    },
+    showModal3 (name, description) {
+      this.coiso.name = name
+      this.coiso.description = description
+      this.$refs.myModalRef3.show()
     },
     closeModal2 () {
       window.location.reload(true)
