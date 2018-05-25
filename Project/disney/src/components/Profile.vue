32607a8415f0d27e6c0f7c325f2241017e3d42c3
Jonas L <joaohccl@hotmail.com>
2018-05-25 02:52:49 +0100

    ...

--- Project/disney/src/components/Profile.vue
+++ Project/disney/src/components/Profile.vue
@@ -150,70 +150,70 @@ export default {
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css?family=Tajawal');
+@import url('https://fonts.googleapis.com/css?family=Tajawal');
 
-  .profile{
-    background-color: #9AB7D3;
-    font-family: 'Tajawal', sans-serif;
-  }
-  .profile_con{
-    padding-top: 5%;
-  }
-  #cover{
-    bottom: 0%;
-  }
-  #cover_pic{
-    opacity: 0.5;
-    height: 500px;
-    width: 100%;
-  }
-  #location{
-    padding-bottom:0px;
-    background-color: transparent;
-    border: none;
-    left: 35%;
-    top: 60%;
-    position: absolute;
-  }
-  #location h4, h5 {
-    font-weight: bold;
-  }
-  #personal_info{
-    top: -20%;
-    left: 7%;
-    height: 600px;
-    background-color:#F7E1D3;
-  }
-  #profile_pic{
-    padding-bottom: 5%;
-  }
-  #button{
-    text-align: center;
-    padding-bottom: 8%;
-  }
-  #follow{
-    padding-left: 8%;
-    padding-right: 8%;
-    color: black;
-    background-color: white;
-  }
-  .main_text{
-    padding-left: 6%;
-    font-weight: bold;
-  }
-  #disney_info h4{
-    padding-left: 8%;
-    padding-top: 10%;
-    font-weight: bold;
-  }
-  #film_info, #character_info{
-    top: 4%;
-    bottom: 20%;
-    left: 6%;
-    padding: 5%;
-    background-color:#F7E1D3;
-  }
-  #character_info{
-    margin-bottom:10%;
-  }
+.profile{
+  background-color: #9AB7D3;
+  font-family: 'Tajawal', sans-serif;
+}
+.profile_con{
+  padding-top: 5%;
+}
+#cover{
+  bottom: 0%;
+}
+#cover_pic{
+  opacity: 0.5;
+  height: 500px;
+  width: 100%;
+}
+#location{
+  padding-bottom:0px;
+  background-color: transparent;
+  border: none;
+  left: 35%;
+  top: 60%;
+  position: absolute;
+}
+#location h4, h5 {
+  font-weight: bold;
+}
+#personal_info{
+  top: -20%;
+  left: 7%;
+  height: 600px;
+  background-color:#F7E1D3;
+}
+#profile_pic{
+  padding-bottom: 5%;
+}
+#button{
+  text-align: center;
+  padding-bottom: 8%;
+}
+#follow{
+  padding-left: 8%;
+  padding-right: 8%;
+  color: black;
+  background-color: white;
+}
+.main_text{
+  padding-left: 6%;
+  font-weight: bold;
+}
+#disney_info h4{
+  padding-left: 8%;
+  padding-top: 10%;
+  font-weight: bold;
+}
+#film_info, #character_info{
+  top: 4%;
+  bottom: 20%;
+  left: 6%;
+  padding: 5%;
+  background-color:#F7E1D3;
+}
+#character_info{
+  margin-bottom:10%;
+}
 </style>
