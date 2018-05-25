0687cf1a66f3aad2a296ff8b694400e793bd92d5
Rui P. Soares <rui.pedro.csoares@gmail.com>
2018-05-25 04:31:33 +0100

    90s done

--- Project/disney/src/components/Decade90s.vue
+++ Project/disney/src/components/Decade90s.vue
@@ -20,24 +20,11 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1991 - Beauty and the Beast</h5>
-                   <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.
-                  </p>
-                  <p>
-                    Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum
-                    sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                  </p>
-                  <div>
-                    <b-img slot="aside" blank blank-color="#ccc" width="64" alt="placeholder" />
+                  <br>
+                  <p>Disney has their first big success in the 90s animation period (thus continuing the Disney Renaissance) with the romantic musical animation <span class="italic">Beauty and the Beast</span>.</p>
+                  <p>Prince Adam was cursed to a beast form by Enchantress who saw no love in his arrogant heart for others. The one way he could break the spell was to learn to love another and earn her love in return before the last petal from his enchanted rose fell, which would bloom until his twenty-first birthday. But who could ever learn to love a beast? Ten years later, Maurice, an inventor from a nearby village, becomes lost in the woods and seeks shelter in the Beast's castle, the Beast imprisons him for trespassing. His daughter Belle, a bookworm who dreams of life outside her provincial village, finds him trapped in the castle and offers her place instead. The Beast accepts with a promise she'll remain in the castle forever. In the beginning Belle views him as nothing more than a monster, he views her as difficult and stubborn. But the two soon taste the bitter-sweetness of finding "you can change and learning you were wrong".</p>
+                  <div class="video">
+                    <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/xDUhINW3SPs" allowfullscreen></b-embed>
                   </div>
                 </b-media>
               </b-card>
@@ -48,13 +35,12 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1992 - Aladdin</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin.
-                  </p>
+                  <br>
+                  <p>In 1992 Disney is on a roll, and release <span class="italic">Alladin</span>. The movie because the most successful film of that year, netting huge profits.</p>
+                  <p>Aladdin is a street urchin who lives in a large and busy town long ago with his faithful monkey friend Abu. When Princess Jasmine gets tired of being forced to remain in the palace that overlooks the city, she sneaks out to the marketplace, where she accidentally meets Aladdin. Under the orders of the evil Jafar (the sultan's advisor), Aladdin is thrown in jail and becomes caught up in Jafar's plot to rule the land with the aid of a mysterious lamp. Legend has it that only a person who is a "diamond in the rough" can retrieve the lamp from the Cave of Wonders. Aladdin might fit that description, but that's not enough to marry the princess, who must (by law) marry a prince.</p>
+                  <div class="video">
+                    <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/hZ1Rb9hC4JY" allowfullscreen></b-embed>
+                  </div>
                 </b-media>
               </b-card>
             </div>
@@ -64,24 +50,11 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1993 - The Nightmare Before Christmas</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.
-                  </p>
-                  <p>
-                    Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum
-                    sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                  </p>
-                  <div>
-                    <b-img slot="aside" blank blank-color="#ccc" width="64" alt="placeholder" />
+                  <br>
+                  <p>Disney continues their Renaissance and releases Tim Burton's masterpiece, <span class="italic">The Nightmare Before Christmas</span>, a dark stop-motion animation about the tale of Jack Skellington.</p>
+                  <p>It is the same routine every year in Halloweentown, on Halloween the monsters come out and perform a real scare. This particular Halloween, the pumpkin king Jack Skellington, bored of the idea, saunters off into the woods with his dog Zero after Halloween night. Upon the break of dawn, he discovers a clearing of trees with different doors representing various holidays. The Christmas Tree door attracts his attention and upon entrance into the world of Christmas, Jack is fascinated with this new idea of Christmas that he must absolutely share with the citizens of Halloweentown.</p>
+                  <div class="video">
+                    <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/kGiYxCUAhks" allowfullscreen></b-embed>
                   </div>
                 </b-media>
               </b-card>
@@ -92,13 +65,11 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1994 - The Lion King</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin.
-                  </p>
+                  <p>In 1994 Disney releases their masterpiece of animation, <span class="italic">The Lion King</span>, climaxing the Disney Renaissance with the highest grossing release of that year and second highest of all time. It net Disney two academy awards, universal acclaim for its story animation and music and a Golden Globe Award.</p>
+                  <p>After being anointed as the next king of the jungle, young lion cub Simba is every bit as eager to take his proud father Mufasa's place as the king. However, this makes Simba's villainous uncle Scar jealous of him. But when Mufasa is suddenly murdered by Scar, Simba feels responsible for his death and runs away from home. Meeting up with two outcasts named Timon and Pumbaa, Simba embraces their ways of 'Hakuna Matata'. Years later, approached by his childhood friend Nala and the wise baboon Rafiki, Simba must return home to stop Scar's evil reign and fulfill his destiny as the new king.</p>
+                  <div class="video">
+                    <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/xB5ceAruYrI" allowfullscreen></b-embed>
+                  </div>
                 </b-media>
               </b-card>
             </div>
@@ -108,13 +79,16 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1995 - Pocahontas & Toy Story</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin.
-                  </p>
+                  <br>
+                  <p>In 1995 Disney releases two big movies, <span class="italic">Pocahontas</span> (based on the woman with the same name) and <span class="italic">Toy Story</span>, a computer-animated adventure film.</p>
+                  <div class="timelineImg">
+                    <img src="../assets/ys.gif" width="400" fluid alt="Rex, Potato Head and Buzz Lightyear."/>
+                    <p>Rex, Mr. Potato Head and Buzz Lightyear.</p>
+                  </div>
+                  <div class="timelineImg">
+                    <img src="../assets/pocahontas.gif" width="400" fluid alt="Pocahontas."/>
+                    <p>Pocahontas.</p>
+                  </div>
                 </b-media>
               </b-card>
             </div>
@@ -124,13 +98,12 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1996 - The Hunchback of Notre Dame</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin.
-                  </p>
+                  <br>
+                  <p>In 1996, Disney releases <span class="italic">The Hunchback of Notre Dame</span>, a darker movie set on the deformed bell ringer of Notre Dame and his struggle to be accepted into society. The movie net good profits and received Academy Awards and Golden Globe nominations.</p>
+                  <p>In 15th century Paris, Clopin the puppeteer tells the story of Quasimodo, the misshapen but gentle-souled bell ringer of Notre Dame, who was nearly killed as a baby by Claude Frollo, the Minister of Justice. But Frollo was forced by the Archdeacon of Notre Dame to raise Quasimodo as his own. Now a young man, Quasimodo is hidden from the world by Frollo in the belltower of the cathedral. But during the Festival of Fools, Quasimodo, cheered on by his gargoyle friends Victor, Hugo, and Laverne, decides to take part in the festivities, where he meets the lovely gypsy girl Esmeralda and the handsome soldier Phoebus. The three of them find themselves ranged against Frollo's cruelty and his attempts to destroy the home of the gypsies, the Court of Miracles. And Quasimodo must desperately defend both Esmeralda and the very cathedral of Notre Dame.</p>
+                  <div class="video">
+                    <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/c4COfl8DMB8" allowfullscreen></b-embed>
+                  </div>
                 </b-media>
               </b-card>
             </div>
@@ -139,14 +112,15 @@
             <div class="timelineElement">
               <b-card class="timelineCard">
                 <b-media>
-                  <h5 class="mt-0">1997 - Hércules</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin.
-                  </p>
+                  <h5 class="mt-0">1997 - Hercules</h5>
+                  <br>
+                  <p>Disney releases an interpretation of greek mythology in <span class="italic">Hercules</span>, representing his Trials and his fight against the evil keeper of the Underworld, Hades. Disney was praised for their representation of Hades and the movie was very well received.</p>
+                  <p>After Zeus fights and sends the Titans to the underworld, he becomes the ruler of Mount Olympus, home of all greek gods. Hades is sent to rule the underworld, but he makes plans to throw Zeus out of Mount Olympus with the Titans' help. The three Fates warn him that his plans would be endangered if Hercules, Zeus newborn child, is still around by the time he tries to take over Mount Olympus. Hades sends Pain and Panic to turn Hercules into a mortal and kill him, but they fail in their attempt and leave him on earth with his divine strength to be raised as a human. Hercules grows up but later learns he is the son of gods, and in order to return to Mt. Olympus he must turn into a "true hero". He is trained by Phil the satyr and becomes the most famous hero in ancient Greece after battling monsters of all kinds. He even saves Mt. Olympus from Hade's take-over, but he only becomes a god again after he offers to exchange his life in order to save Meg, his love, from Hades' underworld and shows that a true hero must have a strong heart and not only physical strength.</p>
+                  <br>
+                  <div class="timelineImg">
+                    <img src="../assets/hades.gif" width="200" fluid alt="Hades"/>
+                    <p>Hades in <span class="italic">Hercules</span></p>
+                  </div>
                 </b-media>
               </b-card>
             </div>
@@ -156,13 +130,16 @@
               <b-card class="timelineCard">
                 <b-media>
                   <h5 class="mt-0">1998 - Mulan & Bug's Life</h5>
-                  <p>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                    in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                    sollicitudin.
-                  </p>
+                  <br>
+                  <p>Disney continues their streak and releases two box offices successes, <span class="italic">Mulan</span> and <span class="italic">Bug's Life</span>.</p>
+                  <div class="timelineImg">
+                    <img src="../assets/mulan.gif" width="300" fluid alt="Mulan"/>
+                    <p>Mulan.</p>
+                  </div>
+                  <div class="timelineImg">
+                    <img src="../assets/caterpillar.gif" width="300" fluid alt="Bug's life."/>
+                    <p>Bug's Life.</p>
+                  </div>
                 </b-media>
               </b-card>
             </div>
